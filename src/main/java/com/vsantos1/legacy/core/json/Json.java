15f588f30517a7a6baffc763929b270ffcafa992
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-20 02:23:11 -0300

    feat: Json object parser

---
+++ src/main/java/com/vsantos1/legacy/core/json/Json.java
@@ -0,0 +1,33 @@
+package com.vsantos1.legacy.core.json;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Json {
+
+    private static final Gson GSON = new Gson();
+    private static final StringBuilder BUILDER = new StringBuilder();
+
+    public Json() {
+    }
+
+    public static <T> T parseToObject(BufferedReader reader, Class<T> target) throws IOException {
+
+        String line;
+
+        for (line = reader.readLine(); line != null; line = reader.readLine()) {
+            BUILDER.append(line);
+        }
+
+        String json = BUILDER.toString();
+        return GSON.fromJson(json, target);
+    }
+
+
+
+    public static String parseToJson(Object object) {
+        return GSON.toJson(object);
+    }
+}
