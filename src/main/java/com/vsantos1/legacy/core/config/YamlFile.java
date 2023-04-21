2fd9f7cf44d2e1c05e52f998b85cafa215d91236
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-20 22:56:45 -0300

    feat: YamlFile config class

---
+++ src/main/java/com/vsantos1/legacy/core/config/YamlFile.java
@@ -0,0 +1,98 @@
+package com.vsantos1.legacy.core.config;
+
+import com.vsantos1.legacy.core.enums.DDL;
+
+public class YamlFile {
+
+    private String url;
+
+    private String host;
+
+    private String port;
+    private String password;
+
+    private String username;
+
+    private String dialect;
+
+    private String ddl;
+
+    private boolean showSql;
+
+    private boolean formatSql;
+
+    private YamlFile() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getdialect() {
+        return dialect;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public void setdialect(String dialect) {
+        this.dialect = dialect;
+    }
+
+    public String getDdl() {
+        return ddl;
+    }
+
+    public void setDdl(String ddl) {
+        this.ddl = ddl;
+    }
+
+    public boolean isShowSql() {
+        return showSql;
+    }
+
+    public void setShowSql(boolean showSql) {
+        this.showSql = showSql;
+    }
+
+    public boolean isFormatSql() {
+        return formatSql;
+    }
+
+    public void setFormatSql(boolean formatSql) {
+        this.formatSql = formatSql;
+    }
+}
