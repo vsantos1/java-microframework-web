2ac22b5160408d3aa41f8202d5421525e0897876
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-20 22:56:59 -0300

    feat: Category model

---
+++ src/main/java/com/vsantos1/legacy/web/models/Category.java
@@ -0,0 +1,64 @@
+package com.vsantos1.legacy.web.models;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_categories")
+public class Category implements Serializable {
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
