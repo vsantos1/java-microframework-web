5d7490d99098dec1b55602357e132858a601287e
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-20 22:57:13 -0300

    feat: Category migration

---
+++ src/main/resources/db/V001_CREATE_CATEGORIES.sql
@@ -0,0 +1,7 @@
+CREATE TABLE  category
+(
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)ENGINE=InnoDB;
