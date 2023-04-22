742f261a5ff8d173188c859db8f6304c5c2e535f
vsantos1 <vinicius.shyn2@gmail.com>
2023-04-21 21:00:47 -0300

    refactor: database connection and yml config file

--- src/main/java/com/vsantos1/legacy/core/db/DatabaseFactory.java
+++ src/main/java/com/vsantos1/legacy/core/db/DatabaseFactory.java
@@ -1,65 +1,39 @@
 package com.vsantos1.legacy.core.db;
 
+import com.vsantos1.legacy.core.config.LoadApplicationConfig;
 import com.vsantos1.legacy.core.config.YamlFile;
 import org.hibernate.cfg.AvailableSettings;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DatabaseFactory {
 
-    private static final Yaml yaml = new Yaml();
 
-    private static String URL;
-    private static String HOST;
-    private static String PORT;
-    private static String DDL;
-
-    private static String SHOW_SQL;
-    private static String FORMAT_SQL;
-    private static String USERNAME;
-    private static String PASSWORD;
-
-    private static String DIALECT;
     private static final String PERSISTENCE_UNIT_NAME = "HireMe";
 
-    public static EntityManagerFactory getEntityManagerFactory() {
+    public static EntityManagerFactory getEntityManagerFactory(String persistenceUnitName) {
 
+        YamlFile yamlFile = LoadApplicationConfig.loadYaml();
+        assert yamlFile != null;
 
         Map<String, String> properties = new HashMap<>();
-        properties.put(AvailableSettings.DIALECT, DIALECT);
-        properties.put(AvailableSettings.HBM2DDL_AUTO, DDL);
-        properties.put(AvailableSettings.SHOW_SQL, SHOW_SQL);
-        properties.put(AvailableSettings.FORMAT_SQL, FORMAT_SQL);
-        properties.put(AvailableSettings.JPA_JDBC_USER, USERNAME);
-        properties.put(AvailableSettings.JPA_JDBC_PASSWORD, PASSWORD);
-        properties.put(AvailableSettings.URL, "jdbc:mysql://localhost:3306/hireme?useTimezone=true&serverTimezone=UTC");
+        properties.put(AvailableSettings.DIALECT, yamlFile.getdialect());
+        properties.put(AvailableSettings.HBM2DDL_AUTO, yamlFile.getDdl());
+        properties.put(AvailableSettings.DRIVER, yamlFile.getDriverClassName());
+        properties.put(AvailableSettings.URL, yamlFile.getUrl());
+        properties.put(AvailableSettings.USER, yamlFile.getUsername());
+        properties.put(AvailableSettings.PASS, yamlFile.getPassword());
+        properties.put(AvailableSettings.SHOW_SQL, String.valueOf(yamlFile.isShowSql()));
+        properties.put(AvailableSettings.FORMAT_SQL, String.valueOf(yamlFile.isFormatSql()));
+        properties.put(AvailableSettings.POOL_SIZE, String.valueOf(10));
 
-        return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, properties);
+        String persistenceUnit = persistenceUnitName == null ? PERSISTENCE_UNIT_NAME : persistenceUnitName;
+        return Persistence.createEntityManagerFactory(persistenceUnit, properties);
     }
 
-    public static void loadYaml() {
-        try {
-            var inputStream = DatabaseFactory.class.getClassLoader().getResourceAsStream("application.yml");
-            YamlFile yamlFile = yaml.loadAs(inputStream, YamlFile.class);
-            HOST = yamlFile.getHost();
-            PORT = yamlFile.getPort();
-            USERNAME = yamlFile.getUsername();
-            PASSWORD = yamlFile.getPassword();
-            DIALECT = yamlFile.getdialect();
-            DDL = yamlFile.getDdl();
-            SHOW_SQL = yamlFile.isShowSql() ? "true" : "false";
-            FORMAT_SQL = yamlFile.isFormatSql() ? "true" : "false";
-            URL = yamlFile.getUrl() == null ? "" : yamlFile.getUrl();
-        } catch (Exception e) {
-            System.out.println("Error loading yaml file: " + e.getMessage());
-        }
-    }
 
 }
