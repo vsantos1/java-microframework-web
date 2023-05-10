916dc8dbac62b30f9b69d4439b126befa787d944
vsantos1 <vinicius.shyn2@gmail.com>
2023-05-10 03:02:42 -0300

    base servlet for check DAO implementations and ResponseBuilder

--- src/main/java/com/vsantos1/legacy/web/servlets/HomeServlet.java
+++ src/main/java/com/vsantos1/legacy/web/servlets/HomeServlet.java
@@ -1,15 +1,17 @@
 package com.vsantos1.legacy.web.servlets;
 
-import com.google.gson.JsonObject;
+import com.vsantos1.legacy.core.dao.GenericDAO;
+import com.vsantos1.legacy.core.encrypt.Encryption;
+import com.vsantos1.legacy.core.encrypt.EncryptionFactory;
 import com.vsantos1.legacy.core.enums.ContentType;
-import com.vsantos1.legacy.core.response.Response;
-
-import com.vsantos1.legacy.web.models.Category;
 import com.vsantos1.legacy.core.enums.HttpStatus;
+import com.vsantos1.legacy.core.response.Response;
+import com.vsantos1.legacy.web.models.Category;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,29 +23,37 @@ public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "A", "Testando"));
-        categories.add(new Category(2L, "B", "Testando"));
-        categories.add(new Category(3L, "C", "Testando"));
-        categories.add(new Category(4L, "D", "Testando"));
-        categories.add(new Category(5L, "F", "Testando"));
-        categories.add(new Category(6L, "G", "Testando"));
 
-        Response.status(HttpStatus.NO_CONTENT).body(categories).build(response, ContentType.APPLICATION_JSON);
+        GenericDAO<Category, Long> genericDAO = new GenericDAO<>();
+
+        List<Category> categories = new ArrayList<>();
+
+        categories.add(new Category(null, "A", "B"));
+        categories.add(new Category(null, "A", "B"));
+        categories.add(new Category(13L, "Lanche da galeras", "dsdas"));
+        categories.add(new Category(19L, "Adsadsa", "Bdsads"));
+        categories.add(new Category(null, "A", "B"));
+        categories.add(new Category(null, "A", "B"));
+        genericDAO.saveAll(categories, Category.class);
+
+        Response.status(HttpStatus.OK).body(categories).build(response, ContentType.APPLICATION_JSON);
+
     }
+
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String name = request.getParameter("name");
         String description = request.getParameter("description");
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("message", "Cadastrado com sucesso!");
-        jsonObject.addProperty("name", name);
-        jsonObject.addProperty("description", description);
 
+        Encryption hash = EncryptionFactory.getInstance(15);
+        String hashed = hash.generateHash(name);
+        GenericDAO<Category, Long> genericDAO = new GenericDAO<>();
 
-        Response.status(HttpStatus.OK).body(jsonObject).build(response, ContentType.APPLICATION_JSON);
+        Category category = genericDAO.save(new Category(null, hashed, description));
+
+        Response.status(HttpStatus.OK).body(category).build(response, ContentType.APPLICATION_JSON);
     }
 
 
