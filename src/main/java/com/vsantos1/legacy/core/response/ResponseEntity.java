5018107ea61c429564892117e41abd582619e78f
vsantos1 <vinicius.shyn2@gmail.com>
2023-05-11 02:47:14 -0300

    chore: response builder content type and renamed

--- src/main/java/com/vsantos1/legacy/core/response/ResponseEntity.java
+++ src/main/java/com/vsantos1/legacy/core/response/ResponseEntity.java
@@ -1,34 +1,34 @@
 package com.vsantos1.legacy.core.response;
 
-import com.vsantos1.legacy.core.enums.ContentType;
-import com.vsantos1.legacy.core.parser.Json;
 import com.vsantos1.legacy.core.enums.HttpStatus;
+import com.vsantos1.legacy.core.parser.Json;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
-public class ResponseBuilder<T> {
+public class ResponseEntity<T> {
     private final HttpStatus status;
     private static final String CHARSET_ENCODING = "UTF-8";
     private final T body;
 
     protected Json json;
-    private final Logger logger = Logger.getLogger(ResponseBuilder.class.getName());
 
-    ResponseBuilder(HttpStatus status, T body) {
+    ResponseEntity(HttpStatus status, T body) {
         this.status = status;
         this.body = body;
     }
 
-    public void build(HttpServletResponse response, ContentType contentType) throws IOException {
+    public void build(HttpServletResponse response) {
+        try {
+            this.execute(response);
 
-        this.execute(response, contentType);
+        } catch (Exception e) {
+        }
     }
 
-    protected void execute(HttpServletResponse response, ContentType contentType) throws IOException {
+
+    protected void execute(HttpServletResponse response) throws IOException {
         response.setStatus(this.status.getValue());
-        response.setContentType(contentType.getValue());
         response.setCharacterEncoding(CHARSET_ENCODING);
 
 
@@ -44,7 +44,6 @@ public class ResponseBuilder<T> {
 
         response.getWriter().write(Json.parseToJson(this.body));
         response.getWriter().close();
-        new Response<>(status, body);
     }
 
 }
